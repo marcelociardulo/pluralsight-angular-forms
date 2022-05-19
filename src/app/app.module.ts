bfb6fb34aa5b9802322e7b4b40aba8a308863973
Marcelo Ciardulo <marcelo.ciardulo@gmail.com>
2022-05-19 15:27:28 -0400

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
+
+@NgModule({
+  declarations: [AppComponent, UserSettingsFormComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
